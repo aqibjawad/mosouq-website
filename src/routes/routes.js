9427c4bb8cd6c6690efd01b569d57e691d610c35
Aqib Jawad <aqibjawad123@gmail.com>
2024-12-11 13:56:55 +0500

    build issuew resilve

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ import Contact from "../pages/contact/index"
 import Category from "../pages/categories";
 import AboutAndContact from "../pages/about/job.about";
 import Blog from "../pages/blogs";
-import BlogDetails from "../pages/blogDetails"
+import BlogDetails from "../pages/blogDetails/index";
 
 import DealDetails from "../pages/dealdetails";
 import Jobs from "../pages/about/job.about"; 
