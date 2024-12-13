f0a87cba04849834a0cdde67e5086623c8ac21e4
Aqib Jawad <aqibjawad123@gmail.com>
2024-12-13 16:49:33 +0500

    html parse description added and all images display

---
+++ src/components/carousal/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import { IoStar } from "react-icons/io5";
+import "./carousel.css";
+
+function CarouselComponent({ businessData }) {
+  return (
+    <div className="business-carousel">
+      {businessData && businessData.images && businessData.images.length > 0 ? (
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          {businessData.images.map((image, idx) => (
+            <SwiperSlide key={idx}>
+              <div className="carousel-item">
+                <img
+                  src={image}
+                  alt={`Business image ${idx + 1}`}
+                  className="d-block w-100"
+                />
+                <div className="carousel-caption">
+                  <div className="rating">
+                    <IoStar color="gold" />
+                    <span>{businessData.rating || "No rating"}</span>
+                  </div>
+                  <h3>{businessData.name}</h3>
+                  <p>{businessData.description}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className="no-images">
+          <p>No images available</p>
+          <div className="carousel-caption">
+            <div className="rating">
+              <IoStar color="gold" />
+              <span>{businessData?.rating || "No rating"}</span>
+            </div>
+            <h3>{businessData?.name}</h3>
+            <p>{businessData?.description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CarouselComponent;
--- src/pages/businessDetails/index.jsx
+++ src/pages/businessDetails/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import CarouselComponent from "../../components/carousel";
+import React, { useState, useEffect, useRef } from "react";
+import FacebookStyleHeader from "../../components/businessHead";
+import CarouselComponent from "../../components/carousal";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { IoStar } from "react-icons/io5";
 import { Rating } from "react-simple-star-rating";
@@ -220,16 +221,19 @@ const BusinessDetails = () => {
       return acc;
     }, {}) || {};
 
+  const isHTML = (str) => /<\/?[a-z][\s\S]*>/i.test(str);
+
   return (
     <div className="property" style={{ marginTop: "1rem" }}>
+      <FacebookStyleHeader
+        businessData={businesses}
+        businessReviewsData={businessReviews}
+      />
+
       <Container fluid className=" py-4 px-3 px-md-5">
         <Row className="g-5">
           <Col lg={8}>
-            <CarouselComponent
-              businessData={businesses}
-              businessReviewsData={businessReviews}
-            />
-
+            <CarouselComponent businessData={businesses} />
             <div
               className=" py-5"
               style={{ borderBottom: "1px solid #F1F1F1" }}
@@ -238,34 +242,21 @@ const BusinessDetails = () => {
                 <div>
                   <h2>Our Services</h2>
                   <div style={{ fontSize: "20px" }} className=" m-0">
-                    {businesses.description}
+                    {isHTML(businesses.description) ? (
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: businesses.description,
+                        }}
+                        style={{ fontSize: "20px" }}
+                      />
+                    ) : (
+                      <p style={{ fontSize: "20px" }}>
+                        {businesses.description}
+                      </p>
+                    )}
                   </div>
                 </div>
-                <div></div>
               </div>
-              {/* <Row>
-                <Col lg={4}>
-                  <Image src="/photo1.png" className="w-100" alt="" />
-                  <div className=" pt-2">
-                    <h5 className=" m-0 fs-5">Dish 1</h5>
-                    <p className=" m-0">Pictures of business uploaded</p>
-                  </div>
-                </Col>
-                <Col lg={4}>
-                  <Image src="/photo2.png" className="w-100" alt="" />
-                  <div className=" pt-2">
-                    <h5 className=" m-0 fs-5">Dish 1</h5>
-                    <p className=" m-0">Pictures of business uploaded</p>
-                  </div>
-                </Col>
-                <Col lg={4}>
-                  <Image src="/photo3.png" className="w-100" alt="" />
-                  <div className=" pt-2">
-                    <h5 className=" m-0 fs-5">Dish 1</h5>
-                    <p className=" m-0">Pictures of business uploaded</p>
-                  </div>
-                </Col>
-              </Row> */}
             </div>
             <div
               className=" py-5"
@@ -299,7 +290,43 @@ const BusinessDetails = () => {
                 </Col>
               </Row>
             </div>
-
+            {/* <div
+              className=" py-5"
+              style={{ borderBottom: "1px solid #F1F1F1" }}
+            >
+              <div className="d-flex pb-4 align-items-center justify-content-between">
+                <div>
+                  <h2>Menu</h2>
+                  <p className=" m-0">Updated Menu of Resturant</p>
+                </div>
+                <div>
+                  <span className="">View full menu</span>
+                </div>
+              </div>
+              <Row>
+                <Col lg={4}>
+                  <Image src="/photo1.png" className="w-100" alt="" />
+                  <div className=" pt-2">
+                    <h5 className=" m-0 fs-5">Dish 1</h5>
+                    <p className=" m-0">Pictures of business uploaded</p>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <Image src="/photo2.png" className="w-100" alt="" />
+                  <div className=" pt-2">
+                    <h5 className=" m-0 fs-5">Dish 1</h5>
+                    <p className=" m-0">Pictures of business uploaded</p>
+                  </div>
+                </Col>
+                <Col lg={4}>
+                  <Image src="/photo3.png" className="w-100" alt="" />
+                  <div className=" pt-2">
+                    <h5 className=" m-0 fs-5">Dish 1</h5>
+                    <p className=" m-0">Pictures of business uploaded</p>
+                  </div>
+                </Col>
+              </Row>
+            </div> */}
             <Modal show={show} onHide={handleClose} className="w-100" centered>
               <Modal.Header className="border-0" closeButton>
                 <Modal.Title>Overall Rating</Modal.Title>
@@ -393,7 +420,37 @@ const BusinessDetails = () => {
                 </div>
               ))}
             </div>
-
+            {/* <div className="pt-4">
+              <Row>
+                {businessReviews.map((review, index) => (
+                  <Col lg={6} key={index}>
+                    <div className="bg-white rounded-3 p-4 mb-3">
+                      <div className="d-flex align-items-center pb-3 justify-content-between">
+                        <div className="d-flex align-items-center">
+                        <span>{review.user.name}</span>
+                          <div className="rating me-2">
+                            {[...Array(5)].map((_, i) => (
+                              <IoStar
+                                key={i}
+                                size={20}
+                                color={
+                                  i < parseInt(review.rating)
+                                    ? "#FFB800"
+                                    : "#D3D3D3"
+                                }
+                              />
+                            ))}
+                          </div>
+                        </div>
+                        <span>{review.rating}</span>
+                      </div>
+                      <h6 className="m-0 mb-2">{review.title}</h6>
+                      <p>{review.description}</p>
+                    </div>
+                  </Col>
+                ))}
+              </Row>
+            </div> */}
             {approvedReviews && approvedReviews.length > 0 ? (
               <div className="pt-4">
                 <Row>
